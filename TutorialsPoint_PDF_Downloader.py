cf5fb6f14946f9c400fcc000ac90fd39d47f6bc6
V!shal <mail007tovishal@gmail.com>
2017-12-25 09:10:11 +0530

    Add files via upload

---
+++ TutorialsPoint_PDF_Downloader.py
@@ -0,0 +1,40 @@
+import wget
+from urllib import request
+from tkinter import *
+
+win =Tk()
+win.geometry ="400x400"
+win.wm_title('TutorialPoint Downloader')
+win.config(background = '#708090', height = 600, width = 450)
+
+def PDF_Download():
+    global tutorial
+    tutorial=tutorial_name.get()
+    try:
+      PDF_url = 'https://www.tutorialspoint.com/' + tutorial + '/' + tutorial + '_tutorial.pdf'
+      print(PDF_url)
+      wget.download(PDF_url)
+
+    except:
+        raise Exception('No such Tutorial Found')
+
+
+
+f= Frame(win,height=500, width=150)
+f.grid(row=0, column=0,padx=50, pady=50)
+tutorial_name = Entry(f, width=70 )
+tutorial_name.grid(row=0, column=0)
+
+Get_PDF = Button(f, text =' Download',command = PDF_Download)
+Get_PDF.grid(row =0, column =1)
+Photo = PhotoImage(file ='tp.png')
+Photo0 = PhotoImage(file = 'b.png')
+lbl0 = Label(f,image=Photo0)
+lbl = Label(f, image = Photo)
+lbl.grid(row =5, column =0,padx=10,pady=10)
+lbl0.grid(row=4,column=0)
+
+win.mainloop()
+
+
+#----------------- Completed to Requirement  --------------------------------------------
