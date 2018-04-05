2fe937b1cca7a3e9659662c6a2cae4a627148d09
V!shal <mail007tovishal@gmail.com>
2018-04-05 08:14:32 +0530

    Scribd Downloader added

---
+++ Books_Downloader.py
@@ -0,0 +1,48 @@
+import bs4 as bs
+from selenium import webdriver
+from urllib import request
+import os
+import re
+import img2pdf
+
+browser = webdriver.Chrome()
+url = 'https://www.scribd.com/document/284732331/Text-Monochrome-Colour-Television-R-R-Gulati-pdf'
+browser.get(url)
+source = browser.page_source
+
+soup = bs.BeautifulSoup(source,"lxml")
+images =[]
+
+for element in soup.find_all('div', attrs={'class':"ie_fix"}):
+    try:
+     images.append(element.find('img').get('src'))
+    except:
+        pass
+
+
+downloader = request.URLopener()
+path = 'C:/Users/Vishal/Desktop/PYTHON BEST/'
+name = url.split('/')[5][0:-23]
+
+newpath = path+name
+
+
+if not os.path.exists(newpath):
+    os.mkdir(name)
+
+for image in images:
+    image_name = image.split('/')[-1]
+    pattern = r"(([0-9]+))"
+    match = re.search(pattern, image_name)
+    try:
+     global page
+     page = match.group()
+
+
+    except:
+        pass
+
+    downloader.retrieve(image,newpath+'/'+page+'.jpg')
+
+with open('output.pdf',"wb") as f:
+    f.write(img2pdf.convert([newpath+'/'+image for image in os.listdir(newpath) if image.endswith('.jpg')]))
